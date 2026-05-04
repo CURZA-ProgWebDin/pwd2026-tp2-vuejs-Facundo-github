8bddafc18bcef8ddd79188d2e6b30b485fc0b5e1
Facundo-github <facundo.contreras92@gmail.com>
2026-05-03 21:13:08 -0300

    Creacion archivo app.vue y componentes.

---
+++ tp2-vue-contreras/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+import {ref, computed} from 'vue';
+import ProductForm from './components/ProductForm.vue';
+import ProductList from './components/ProductList.vue';
+
+</script>
+
+<template>
+
+</template>
