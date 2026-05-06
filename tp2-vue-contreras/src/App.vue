1f59df9791bedc164aa2cfd1cbe04b7ec6af5482
Facundo-github <facundo.contreras92@gmail.com>
2026-05-06 17:14:04 -0300

    Logica app y productForm

--- tp2-vue-contreras/src/App.vue
+++ tp2-vue-contreras/src/App.vue
@@ -4,8 +4,38 @@ import {ref, computed} from 'vue';
 import ProductForm from './components/ProductForm.vue';
 import ProductList from './components/ProductList.vue';
 
+const state = reactive({
+    productos: [],
+    filtro: 'Todos'
+})
+
+let idCounter = 1
+
+function eliminarProducto(id) {
+    state.productos = state.productos.filter(p => p.id !== id)
+}
+
+const ProductosFiltrados = computed(() => {
+    if (state.filtro === 'Todos') return state.productos
+    return state.productos.filter(p => p.categoria === state.filtro)
+})
+
+const resumen = computed(() => ({
+    total: state.productos.length,
+    valorInventario: state.productos.reduce((acc, p) => acc + p.precio * precio.stock , 0) 
+}))
+
 </script>
 
 <template>
+
+    <h1>Gestion de Productos</h1>
+    <ProductForm @crear="crearProducto" />
+    <ProductList
+        :productos="ProductosFiltrados"
+        @eliminar="eliminarProducto"
+    />
+    <p>Total Productos: {{ resumen.total }}</p>
+    <p>Valor Inventario: {{ resumen.valorInventario }}</p>
 
 </template>
---
+++ tp2-vue-contreras/src/components/ProductForm.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+const nombre = ref('')
+const precio = ref('')
+const stock = ref('')
+const categoria = ref('')
+
+const emit = defineEmits(['crear'])
+
+function submitForm() {
+    if (!nombre.value || !precio.value || !stock.value || !categoria.value) {
+        alert('Todos los campos son obligatorios')
+        return
+    }
+    emit('crear', {
+        nombre: nombre.value,
+        precio: Number(precio.value),
+        stock: Number(stock.value),
+        categoria: categoria.value
+    })
+    nombre.value = precio.value = stock.value = categoria.value = ''
+}
+
+</script>
+
+<template>
+
+    <form @submit.prevent="submitForm">
+        <input v-model="nombre" placeholder="Nombre" />
+        <input v-model="precio" type="number" placeholder="precio" />
+        <input v-model="stock" type="number" placeholder="Stock"/>
+        <select v-model="categoria">
+            <option disabled value="">Seleccione Categoria</option>
+            <option>Electronica</option>
+            <option>Ropa</option>
+            <option>Alimentos</option>  
+        </select>
+        <button type="submit">Agregar</button>        
+    </form>
+
+</template>
