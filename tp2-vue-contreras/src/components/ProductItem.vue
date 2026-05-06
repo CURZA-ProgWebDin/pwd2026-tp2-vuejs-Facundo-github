babb8653fddecf199d6211e6f4d23b163c6ef55e
Facundo-github <facundo.contreras92@gmail.com>
2026-05-06 17:59:08 -0300

    logica item y list

---
+++ tp2-vue-contreras/src/components/ProductItem.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({producto: Object })
+const emit = defineEmits(['eliminar'])
+</script>
+
+<template>
+    <li>
+        {{ producto.nombre }} - ${{ producto.precio }} - Stock:{{ producto.stock }} - 
+        <button @click="emit('elimnar', producto.id)">Eliminar</button>
+    </li>
+</template>
+
+<style>
+</style>
---
+++ tp2-vue-contreras/src/components/ProductList.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import ProductItem from './ProductItem.vue';
+const props = defineProps({productos: Array})
+const emit = defineEmits(['eliminar'])
+
+</script>
+
+<template>
+
+    <ul>
+        <ProductItem
+        v-for="p in productos"
+        :key="p.id"
+        :producto="p"
+        @eliminar="emit('eliminar', p.id)"
+        />
+    </ul>
+
+</template>
+
+<style>
+</style>
